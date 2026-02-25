8133f96cc9c13029565d5aa829e21224ac46f565
Jack Williams <Jack.Williams@keyin.com>
2026-02-25 15:42:01 -0330

    Added first version of code for InputValidation.py

--- InputValidation.py
+++ InputValidation.py
@@ -1,1 +1,12 @@
 # Code to validate user inputs #
+
+def inputValidation(numChoices, userInput):
+    loop = True
+    while loop == True:
+        if 0 < int(userInput) <= numChoices:
+            loop == False
+            return userInput
+        elif int(userInput) == ValueError:
+            userInput == input(f'Please enter a valid number (1 - {numChoices}) ')
+        else:
+            userInput == input(f'Please enter a valid number (1 - {numChoices}) ')
