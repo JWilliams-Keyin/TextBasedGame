b1920663acf3965e2e94541f859477681f362c53
Jack Williams <Jack.Williams@keyin.com>
2026-02-27 19:26:00 -0330

    Reworked InputValidation.py to contain the input statements

--- InputValidation.py
+++ InputValidation.py
@@ -1,12 +1,12 @@
 # Code to validate user inputs #
 
-def validate(numChoices, userInput):
+def playerInput(numChoices, prompt):
     loop = True
     while loop == True:
-        if 0 < int(userInput) <= numChoices:
-            loop == False
-            return userInput
-        elif int(userInput) == ValueError:
-            userInput == input(f'Please enter a valid number (1 - {numChoices}) ')
-        else:
-            userInput == input(f'Please enter a valid number (1 - {numChoices}) ')
+        try:
+            userInput = int(input(f'{prompt} '))
+            loop = False
+        except ValueError:
+            print(f'Invalid input. Please input a valid number (1 - {numChoices})')
+        except 0 < int(userInput) < int(numChoices):
+            print(f'Invalid input. Please input a valid number (1 - {numChoices})')
